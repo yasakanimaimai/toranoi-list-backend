691719ee6ba84d5a0363528622545ce5f9ce269d
fujitaiori <ioriori0413@gmail.com>
2022-12-14 18:56:53 +0900

    user操作を追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { AuthModule } from './auth/auth.module';
 import { ArticleModule } from './article/article.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    AuthModule, ArticleModule, PrismaModule],
+    AuthModule, ArticleModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async updateUser(
+    userId: string,
+    dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto
+      },
+    });
+    delete user.hashedPassword;
+    return user
+  }
+}
